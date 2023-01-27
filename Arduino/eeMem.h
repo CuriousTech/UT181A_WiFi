9f144fc2a8fe9a4b287fe39c2f8ce7ae6192e712
Greg Cunningham <greg@curioustech.net>
2023-01-27 03:44:21 -0500

    Struct merge

--- Arduino/eeMem.h
+++ Arduino/eeMem.h
@@ -3,21 +3,7 @@
 
 #include <Arduino.h>
 
-struct eeSet // EEPROM backed data
-{
-  uint16_t size;          // if size changes, use defauls
-  uint16_t sum;           // if sum is diiferent from memory struct, write
-  char     szSSID[32];
-  char     szSSIDPassword[64];
-  int8_t   tz;            // Timezone offset
-  uint8_t  dst;
-  bool     bEnableOLED;
-  uint8_t  exportFormat;
-  uint16_t rate;
-  uint32_t updateTime;
-};
-
-extern eeSet ee;
+#define EESIZE (offsetof(eeMem, end) - offsetof(eeMem, size) )
 
 class eeMem
 {
@@ -26,8 +12,20 @@ public:
   void update(void);
 private:
   uint16_t Fletcher16( uint8_t* data, int count);
+public:
+  uint16_t size;          // if size changes, use defauls
+  uint16_t sum;           // if sum is diiferent from memory struct, write
+  char     szSSID[32] = "";
+  char     szSSIDPassword[64] = "";
+  int8_t   tz = -5;            // Timezone offset
+  uint8_t  dst = 0;
+  bool     bEnableOLED = false;
+  uint8_t  exportFormat = 0;
+  uint16_t rate = 0;
+  uint32_t updateTime = 0;
+  uint8_t  end;
 };
 
-extern eeMem eemem;
+extern eeMem ee;
 
 #endif // EEMEM_H
